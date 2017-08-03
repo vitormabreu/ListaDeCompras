8467156e822995bb46e36afce05e099598d1cfac
Vitor Abreu <vitinabreu@gmail.com>
2017-08-03 17:52:27 -0300

    Start app.js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('groceryListApp', []);
+
+app.controller('HomeController', [$scope, function ($scope) {
+
+    $scope.appTitle = 'Lista de Compras';
+
+}])
+
+app.controller('ListItemsController', [$scope, function ($scope) {
+
+    $scope.groceryItems = [
+        {completed: true, itemName: 'Leite', date: '03/08/2017'},
+        {completed: true, itemName: 'Pão', date: '03/08/2017'},
+        {completed: true, itemName: 'Ovos', date: '03/08/2017'},
+        {completed: true, itemName: 'Farinha', date: '03/08/2017'},
+        {completed: true, itemName: 'Óleo', date: '03/08/2017'},
+        {completed: true, itemName: 'Manteiga', date: '03/08/2017'},
+        {completed: true, itemName: 'Carne', date: '03/08/2017'},
+        {completed: true, itemName: 'Arroz', date: '03/08/2017'},
+        {completed: true, itemName: 'Feijão', date: '03/08/2017'},
+    ]
+
+}]);
